e9fa6401f3977a4b917c32a371c7d4252aacd5af
Marko Simon <marko@ilert.com>
2023-09-01 17:11:24 +0200

    add alert action playground + readme

--- alert-actions/README.md
+++ alert-actions/README.md
@@ -1,5 +1,43 @@
 # Alert actions playground
 
-## How to run?
+The provided default example in `alert-action.tf` creates a sample user, an escalation policy and an alert source. Additionally, the alert source features three alert actions (email, webhook, GitHub), whereas GitHub additionally connects with a GitHub connector.
 
-TODO
+> Note that those alert actions are provided with example credentials and do not work properly.
+
+## Setup
+
+1. Create `terraform.tfvars` file in current directory and enter your ilert API key. Example:
+
+   ```
+   api_token = "your api key"
+   ```
+
+   > You can create an api key in your ilert account under `User icon -> Manage API keys -> > Add API key`
+
+2. Open terminal and navigate to current directory
+
+   ```sh
+   $ cd alert-actions/
+   ```
+
+3. Initialize the ilert Terraform provider, by executing the following command:
+
+   ```sh
+   $ terraform init
+   ```
+
+## Create/Edit resources
+
+1. Edit `alert-action.tf` to declare the resources that should be created/edited in ilert
+
+2. (Optional) Execute `terraform plan` to preview changed made and locate possible errors beforehand`
+
+   ```sh
+   $ terraform plan
+   ```
+
+3. Run `terraform apply` to create or edit the resources
+
+   ```sh
+   $ terraform apply
+   ```
---
+++ alert-actions/alert-action.tf
@@ -0,0 +1,147 @@
+resource "ilert_user" "example" {
+  first_name = "example"
+  last_name  = "example"
+  email      = "example@example.com"
+}
+
+resource "ilert_escalation_policy" "example" {
+  name = "example"
+  escalation_rule {
+    escalation_timeout = 15
+    user               = ilert_user.example.id
+  }
+}
+
+resource "ilert_alert_source" "example" {
+  name              = "My Grafana Integration for GitHub"
+  integration_type  = "GRAFANA"
+  escalation_policy = ilert_escalation_policy.example.id
+}
+
+resource "ilert_connector" "example" {
+  name = "My GitHub Connector"
+  type = "github"
+
+  github {
+    api_key = "my api key"
+  }
+}
+
+resource "ilert_alert_action" "example_github" {
+  name = "github"
+
+  alert_source {
+    id = ilert_alert_source.example.id
+  }
+
+  connector {
+    id   = ilert_connector.example.id
+    type = ilert_connector.example.type
+  }
+
+  github {
+    owner      = "my org"
+    repository = "my repo"
+  }
+
+  alert_filter {
+    operator = "AND"
+    predicate {
+      field    = "ALERT_SUMMARY"
+      criteria = "CONTAINS_ANY_WORDS"
+      value    = "EXAMPLE"
+    }
+    predicate {
+      field    = "ALERT_DETAILS"
+      criteria = "CONTAINS_NOT_WORDS"
+      value    = "EXAMPLE"
+    }
+    predicate {
+      field    = "ESCALATION_POLICY"
+      criteria = "IS_STRING"
+      value    = ilert_escalation_policy.example.id
+    }
+    predicate {
+      field    = "ALERT_PRIORITY"
+      criteria = "IS_STRING"
+      value    = "HIGH"
+    }
+  }
+}
+
+resource "ilert_alert_action" "example_email" {
+  name = "email"
+  alert_source {
+    id = ilert_alert_source.example.id
+  }
+  connector {
+    type = "email"
+  }
+  email {
+    recipients = ["example@example.com"]
+    subject    = "alo"
+  }
+  trigger_types = ["alert-created"]
+
+  alert_filter {
+    operator = "OR"
+    predicate {
+      field    = "ALERT_SUMMARY"
+      criteria = "IS_STRING"
+      value    = "ALO"
+    }
+    predicate {
+      field    = "ALERT_DETAILS"
+      criteria = "IS_NOT_STRING"
+      value    = "ALO"
+    }
+    predicate {
+      field    = "ALERT_DETAILS"
+      criteria = "MATCHES_REGEX"
+      value    = "ALO"
+    }
+    predicate {
+      field    = "ALERT_SUMMARY"
+      criteria = "MATCHES_NOT_REGEX"
+      value    = "ALO"
+    }
+  }
+}
+
+resource "ilert_alert_action" "example_webhook" {
+  name = "webhook"
+  alert_source {
+    id = ilert_alert_source.example.id
+  }
+  connector {
+    type = "webhook"
+  }
+  trigger_mode = "MANUAL"
+  webhook {
+    url = "https://www.google.com/"
+  }
+
+  alert_filter {
+    operator = "OR"
+    predicate {
+      field    = "ALERT_SUMMARY"
+      criteria = "IS_STRING"
+      value    = "EXAMPLE"
+    }
+    predicate {
+      field    = "ALERT_DETAILS"
+      criteria = "IS_NOT_STRING"
+      value    = "EXAMPLE"
+    }
+    predicate {
+      field    = "ALERT_DETAILS"
+      criteria = "MATCHES_REGEX"
+      value    = "EXAMPLE"
+    }
+    predicate {
+      field    = "ALERT_SUMMARY"
+      criteria = "MATCHES_NOT_REGEX"
+      value    = "EXAMPLE"
+    }
+  }
+}
